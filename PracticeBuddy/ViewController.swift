78c4fb1f128e6cfe0c96ec5e5c70a45594ef898b
Tyler Hostager <tyhostager@gmail.com>
2018-04-03 06:02:30 -0500

    Initial Commit

---
+++ PracticeBuddy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PracticeBuddy
+//
+//  Created by Tyler Hostager on 4/3/18.
+//  Copyright © 2018 PB Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
